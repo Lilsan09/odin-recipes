d044d9db9095df74c4bc5572bd2d9e2aa1674abe
Alexis Ferrer <alexisferrer60160@gmail.com>
2022-06-06 15:02:14 +0200

    suppression des console.log et ajout des commentaires

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,10 @@
-console.log('js loadeds')
+///////// Bouton Lasagne /////////
 
 const lasagne = document.getElementById('lasagne')
 
 const lasagne_de_base = lasagne.innerHTML
 
-console.log(lasagne)
-
 lasagne.addEventListener('mouseenter', e =>{
-   // lasagne.innerHTML = "<button><a href='../odin-recipes/recettes/lasagne.html'>45 min</a></button>"
     lasagne.innerHTML = ""
 let button = document.createElement('a')
 button.href = "../odin-recipes/recettes/lasagne.html"
@@ -17,6 +14,23 @@ lasagne.appendChild(button)
 })
 
 lasagne.addEventListener('mouseleave', e =>{
-    console.log('coucou')
     lasagne.innerHTML = lasagne_de_base
+})
+
+/////// Bouton Grattin de courgette ////////
+
+const grattin = document.getElementById('grattin')
+
+const grattin_de_base = grattin.innerHTML
+
+grattin.addEventListener('mouseenter', e =>{
+    grattin.innerHTML = ""
+let button = document.createElement('a')
+button.href = "../odin-recipes/recettes/grattincourgette.html"
+button.innerHTML = "20 min"
+grattin.appendChild(button)
+})
+
+grattin.addEventListener('mouseleave', e =>{
+    grattin.innerHTML = grattin_de_base
 })
